97c6ef34dba18706f40af98f2fd57574eb3a4ff0
MuhammadBhakti <muhammadbhaktisetiawan@gmail.com>
2021-02-27 23:31:18 +0700

    tambahan disaat santai

--- ds3_tidyr.R
+++ ds3_tidyr.R
@@ -1,2 +1,18 @@
 #Data cleaning and preparation
-library(tidyr)
+library(tidyr) #or
+library(tidyverse)
+
+#using default dataset
+
+# 1.Gather
+view(table4a)
+
+table4a %>% gather('1999','2000', key = "year", value = "case")
+
+table4a %>% gather('1999','2000', key = "year", value = "population")
+
+
+# 2.spreading
+view(table2)
+
+table2 %>% spread(key = type, value = count)
